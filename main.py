116173e1221ce27671d6ccd7319eac4d8ba9d114
_______ <53409082+xannyyyy@users.noreply.github.com>
2020-03-06 17:24:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,18 @@ def AddConn(name, token, amount):
             time.sleep(10) # 10 seconds sleep time so you dont flood discord requests
             r = requests.put(f'https://canary.discordapp.com/api/v6/users/@me/connections/{type}/{ID}', data=json.dumps(payload), headers=headers)
             if r.status_code == 200:
-                print("New connection added!")
+                print(f"{Fore.GREEN}[+] New connection added!"+Fore.RESET)
+            elif 'Unauthorized' in r.text:
+                print(f"{Fore.RED}[-] Improper token has been passed!"+Fore.RESET);break
             else:
-                print("Couldnt add connection :(")  
-        except Exception as e:
+                print(f"{Fore.RED}[-] Couldnt add connection :("+Fore.RESET)
+        except (Exception, ValueError) as e:
             print(e)
-        else:
-            print(r.text);break
 
 if __name__ == '__main__':
     print("-----------------------------------")
     name = input("What do you want the name of the connection to be?\n> ")
     token = input("Paste your discord user token here\n> ")
     amount = int(input("How many connections do you want?\n> "))
-    print(f"{Fore.RED}WARNING: {Fore.RESET}Close this program when you feel like you have enough connections!")
     print("-----------------------------------")
     AddConn(name, token, amount)
