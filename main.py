9e49d96740d82d85968ca46a81549783f13a2bc2
_______ <53409082+xannyyyy@users.noreply.github.com>
2020-03-06 17:19:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ def AddConn(name, token, amount):
 
 if __name__ == '__main__':
     print("-----------------------------------")
-    name = input("What do you want the name of the connection be?\n>")
-    token = input("Paste your discord user token here\n>")
-    amount = int(input("How many connections do you want?\n>"))
+    name = input("What do you want the name of the connection to be?\n> ")
+    token = input("Paste your discord user token here\n> ")
+    amount = int(input("How many connections do you want?\n> "))
     print(f"{Fore.RED}WARNING: {Fore.RESET}Close this program when you feel like you have enough connections!")
     print("-----------------------------------")
     AddConn(name, token, amount)
